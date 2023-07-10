56d247b2147e1200f4805e11489e98fe77004238
Алексей Туровский <a.turowskiy@gmail.com>
2023-07-10 12:12:03 +0700

    Implement getting file path from cmd

--- pdf_image_exporter.py
+++ pdf_image_exporter.py
@@ -27,18 +27,16 @@ class ExportedImageFile:
         """Get the hash of the image."""
         return imagehash.average_hash(self.image)
 
-    def save_image(self):
+    def save_image(self, path: Path):
         """Save image to a file."""
-        self.image.save(self.name.split('.')[0] + '.png')
+        self.image.save(str(path) + '/' + self.name.split('.')[0] + '.png')
 
 
-images = {}
-
-
-def extract_images():
+def extract_images(pdf_file: Path) -> dict:
     """Extract images from"""
+    images = {}
     count = 0
-    reader = PdfReader("Pawns.pdf")
+    reader = PdfReader(pdf_file)
     for page_num, page in enumerate(reader.pages):
         for image_file_object in page.images:
             temp_name = (str(page_num) + '_' + str(count)
@@ -46,9 +44,29 @@ def extract_images():
             temp = ExportedImageFile(image_file_object.data, temp_name)
             images[temp.get_hash()] = temp
             count += 1
-    print(len(images))
-    for image in images.values():
-        image.save_image()
+    return images
 
 
-extract_images()
+def main():
+    try:
+        pdf_file = Path(sys.argv[1])
+    except IndexError:
+        print('No pdf file!')
+        print(__doc__)
+        exit()
+    # directory for images, 'pdf file name' + _export
+    save_dir = Path(str(pdf_file.parent / pdf_file.stem) + '_export')
+    print('Saving images in', save_dir)
+    try:
+        save_dir.mkdir(exist_ok=True)
+    except FileNotFoundError:
+        print('Can\'t create dir', save_dir)
+
+    exported_images = extract_images(pdf_file)
+
+    for image in exported_images.values():
+        image.save_image(save_dir)
+    print(f'Saved {len(exported_images)} unique images.')
+
+
+main()
