f15b2ffce7f81d8856359be44b57b7e45b842ae4
a.turovskiy <a.turowskiy@gmail.com>
2023-09-29 09:34:23 +0700

    Add conversion to RGB before saving

--- pdf_image_exporter.py
+++ pdf_image_exporter.py
@@ -29,7 +29,9 @@ class ExportedImageFile:
 
     def save_image(self, path: Path) -> None:
         """Save image to a file."""
-        self.image.save(str(path) + '/' + self.name.split('.')[0] + '.png')
+        self.image.convert('RGB').save(
+            str(path) + '/' + self.name.split('.')[0] + '.png'
+        )
 
 
 def extract_images(pdf_file: Path) -> dict[str: ExportedImageFile]:
